a4ea9ab338425329d8e22fa40fd847311fbe4a74
OliverLiy <974474148@qq.com>
2020-12-20 21:56:53 +0800

    first commit

---
+++ src/main/java/com/javayz/verificationcode/VerificationcodeApplication.java
@@ -0,0 +1,13 @@
+package com.javayz.verificationcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerificationcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VerificationcodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javayz/verificationcode/VerificationcodeApplicationTests.java
@@ -0,0 +1,11 @@
+package com.javayz.verificationcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerificationcodeApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
